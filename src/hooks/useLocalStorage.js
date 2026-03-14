56a6b0a93057c4471210b5a4f37819577123324a
Israel-machine <israel.diaz1701@gmail.com>
2026-03-14 14:44:46 -0700

    local storage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,16 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage(key, initialValue = null) {
+    const [state, setState] = useState(() => {
+        const savedValue = window.localStorage.getItem(key);
+        if (savedValue === null) return initialValue;
+        return savedValue
+    });
+
+    useEffect(() => {
+        const valueToStore = state;
+        window.localStorage.setItem(key, valueToStore);
+    }, [key, state]);
+
+    return [state, setState];
 }
